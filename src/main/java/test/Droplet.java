7538de7b48ff01639140a730fd5e85a760b3f7aa
mabrkr <s185039@student.dtu.dk>
2020-04-18 16:48:16 +0200

    Create and Delete tested and working

---
+++ src/main/java/test/Droplet.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Droplet {
+
+    private String name = "Example.com";
+    private String region = "nyc3";
+    private String size = "s-1vcpu-1gb";
+    private String image = "ubuntu-16-04-x64";
+}
