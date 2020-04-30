6a8b88871f5b30b803344beed26b430238265a90
mabrkr <s185039@student.dtu.dk>
2020-04-30 10:53:01 +0200

    user and database controllers + endpoints started 4

--- src/main/java/DAO/DatabaseConnection.java
+++ src/main/java/DAO/DatabaseConnection.java
@@ -8,7 +8,7 @@ public class DatabaseConnection {
 
     private static Connection conn;
 
-    private static Connection getConnection() {
+    private static Connection getConnection() { // TODO: Burde den være synchronized?
         try {
             if (conn == null || conn.isClosed()) {
                 String hostname = "127.0.0.1:3306/?serverTimezone=UTC#";
