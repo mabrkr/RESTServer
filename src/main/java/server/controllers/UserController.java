ffc8125c7e38c1be5c095644f590536fcbe8f6b8
mabrkr <s185039@student.dtu.dk>
2020-05-02 15:27:05 +0200

    Authentication using the database and Basic Authorization headers implemented. The way connections to the database are established might be able to be improved.

--- src/main/java/server/controllers/UserController.java
+++ src/main/java/server/controllers/UserController.java
@@ -5,17 +5,17 @@ import io.javalin.http.Handler;
 public class UserController {
 
     public Handler getUser = ctx -> {
-        // Hent brugerdata fra database og returner
+        // Hent brugerdata fra database og returner.
     };
 
     public Handler newUser = ctx -> {
-        // Opret ny bruger i databasen
+        // Opret ny bruger i databasen.
     };
 
     public Handler updateUser = ctx -> {
-        // Opdater bruger. Relevant ift. skift af kodeord og indtastning af API-nøgler
+        // Opdater bruger. Relevant ift. skift af kodeord.
     };
     public Handler addApiKey = ctx -> {
-
+        // Tilføj api nøgle til bruger. Request body skal indeholde nødvendig info (host/registrar og nøgle).
     };
 }
